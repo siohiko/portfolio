8b1511004d3586c226061650bfe89138ef359e71
siokawa yuusuke <siokawa0476@gmail.com>
2020-12-13 22:28:08 +0900

    add authenticate_user before users#show

--- rails_app/spec/requests/users_request_spec.rb
+++ rails_app/spec/requests/users_request_spec.rb
@@ -18,12 +18,24 @@ RSpec.describe "Users", type: :request do
     it { subject; expect(response).to have_http_status(code)}
   end
 
+  shared_examples_for "redirect to login_path" do
+    it { subject; expect(response).to redirect_to new_user_session_path }
+  end
 
   describe "GET /show" do
     subject { get user_path(registered_user.user_id) }
-    let(:registered_user) { create(:valid_user) }
 
-    it_behaves_like "return http", 200
+    context 'case of Logged in' do
+      let(:registered_user) { create(:valid_user) }
+      before { sign_in registered_user }
+      it_behaves_like "return http", 200
+    end
+    
+    context 'case of being not Logged in' do
+      let(:registered_user) { create(:valid_user) }
+      it_behaves_like "return http", 302
+      it_behaves_like "redirect to login_path"
+    end
+
   end
-
 end
