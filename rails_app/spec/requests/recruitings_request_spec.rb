ab76d210e66e11954e6bf2f4e80e1ac266ecddc5
siokawa yuusuke <siokawa0476@gmail.com>
2021-01-19 15:39:44 +0900

    fix recruiting controller

--- rails_app/spec/requests/recruitings_request_spec.rb
+++ rails_app/spec/requests/recruitings_request_spec.rb
@@ -28,6 +28,10 @@ RSpec.describe "Recruitings", type: :request do
     it { subject; expect(response).to redirect_to edit_recruiting_path(registered_user.recruiting.id) }
   end
 
+  shared_examples_for "redirect to root_path" do
+    it { subject; expect(response).to redirect_to root_path }
+  end
+
 
 
 
@@ -102,17 +106,17 @@ RSpec.describe "Recruitings", type: :request do
       context 'with invalid value of type' do
         let(:params) { attributes_for(:valid_recruiting, type: "no_game") }
 
-        it_behaves_like "return http", 200
+        it_behaves_like "return http", 302
+        it_behaves_like "redirect to root_path"
         it_behaves_like "Failing to create Model", Recruiting
-        it_behaves_like "include error message", '指定のゲームは募集できません'
       end
 
       context 'without type' do
         let(:params) { attributes_for(:valid_recruiting, type: nil) }
 
         it_behaves_like "return http", 200
+        it_behaves_like "include error message", '募集するゲーム を入力してください'
         it_behaves_like "Failing to create Model", Recruiting
-        it_behaves_like "include error message", '指定のゲームは募集できません'
       end
 
       context 'without recruitment_numbers' do
@@ -245,7 +249,7 @@ RSpec.describe "Recruitings", type: :request do
           let(:params) { attributes_for(:valid_recruiting, type: nil) }
   
           it_behaves_like "return http", 200
-          it_behaves_like "include error message", '指定のゲームは募集できません'
+          it_behaves_like "include error message", '募集するゲーム を入力してください'
         end
   
         context 'without recruitment_numbers' do
